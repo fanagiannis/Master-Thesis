918ec4ff455ebbf8129fc4694a965b125c454ad1
fanagiannis <foulis67@gmail.com>
2024-10-23 20:46:42 +0300

    More CleanUp

--- Items/Weapons/WeaponSlot.cs
+++ Items/Weapons/WeaponSlot.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponSlot : MonoBehaviour
-{
-    [SerializeField]private WeaponManager equippedweapon;
-    private void Start()
-    {
-        SetWeapon();
-    } 
-    public void SetWeapon()
-    {
-        equippedweapon = GetComponentInChildren<WeaponManager>();
-    }
-    public void FireEquippedWeapon()
-    {
-        equippedweapon.Fire(GetComponentInParent<Camera>().transform);
-    }
-}
+// public class WeaponSlot : MonoBehaviour
+// {
+//     [SerializeField]private WeaponManager equippedweapon;
+//     private void Start()
+//     {
+//         SetWeapon();
+//     } 
+//     public void SetWeapon()
+//     {
+//         equippedweapon = GetComponentInChildren<WeaponManager>();
+//     }
+//     public void FireEquippedWeapon()
+//     {
+//         equippedweapon.Fire(GetComponentInParent<Camera>().transform);
+//     }
+// }
