9974b2b2b1eb57bd35a519aba74dc86f97316cd6
fanagiannis <foulis67@gmail.com>
2024-10-24 09:46:12 +0300

    Added Player and Movement

---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed; 
+    [SerializeField]private CharacterController Controller;
+    [SerializeField]private PlayerInput playerInput;
+    void Start()
+    {
+        Controller=GetComponent<CharacterController>();
+        playerInput=this.gameObject.GetComponent<PlayerInput>();
+        playerInput.actions.Enable();
+    }
+    void Update()
+    {   
+        Movement();
+        Look();
+    }
+    
+    private void Movement()
+    {
+        float Move_X=playerInput.actions["Movement"].ReadValue<Vector2>().x;//Input.GetAxis("Horizontal");
+        float Move_Z=playerInput.actions["Movement"].ReadValue<Vector2>().y;//Input.GetAxis("Vertical");
+        Controller.Move(new Vector3(Move_X,0,Move_Z)*moveSpeed*Time.deltaTime);
+    }
+    private void Look()
+    {
+         Vector2 mouseInput = playerInput.actions["Look"].ReadValue<Vector2>();
+        float mouseX = mouseInput.x * 10f * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+    }
+    public PlayerInput PlayerInput(){return playerInput;}
+    public bool HasJumped(){return !Controller.isGrounded; }
+}
